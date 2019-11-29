a67e8a33b0e8ed2d00e0d36152db9f5bc2b598ef
Matheus Moraes <matheusmoraes@MacBook-de-Matheus.local>
2019-11-29 10:45:17 -0300

    att

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+$(".dropdown-container").hide();
+
+
+$(".dropdown-btn#institucional").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#institucional i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#institucional").slideDown()
+}, () => {
+    $(".dropdown-container#institucional").hide()
+    $(".dropdown-btn#institucional i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#suporte").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#suporte i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#suporte").slideDown()
+}, () => {
+    $(".dropdown-container#suporte").hide()
+    $(".dropdown-btn#suporte i").attr('class', 'fa fa-angle-down');
+})
--- js/pagination.js
+++ js/pagination.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".pages li.previous", "body").html("<img src='/arquivos/bola1.png'/>")
-    $(".pages li.next", "body").html("<img src='/arquivos/bola2.png'/>")
+    $(".pages li.previous", "body").html("<img src='/arquivos/bola1.png' style='max-width:80%'/>")
+    $(".pages li.next", "body").html("<img src='/arquivos/bola2.png' style='max-width:80%'/>")
     
 });
