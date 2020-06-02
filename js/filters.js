e424503e7bba40819c2005b964054eb8491960f2
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-06-02 18:18:00 -0300

    a

---
+++ js/filters.js
@@ -0,0 +1,18 @@
+
+
+setTimeout(function(){
+    $("#admake-advanced-filter > .box-filtro h3").append(`<span class="caret" style="margin-left:16px;float: right;margin-top: 8px;">`);
+
+   // $("#admake-advanced-filter .opcoes").hide();
+    
+    $("#admake-advanced-filter > .box-filtro h3").toggle(function() {
+        let me = $(this).text().replace(/\s+/g, '-').toLowerCase();
+        setTimeout(()=>{
+            $(`#admake-advanced-filter > .box-filtro.filtro-${me} .opcoes`).slideDown();
+        },1000)
+        
+    }, function() {
+        let me = $(this).text().replace(/\s+/g, '-').toLowerCase();
+        $(`#admake-advanced-filter > .box-filtro.filtro-${me} .opcoes`).slideUp();
+    })
+}, 1000)
