6048a2db4e19443f77d5ac4a81cc79ec79ce2180
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-08-12 15:42:27 -0300

    mm

---
+++ js/newsletter.js
@@ -0,0 +1,25 @@
+
+
+
+$("#commit").click(()=>{
+    event.preventDefault(); 
+    //console.log($("#cl_email").val())
+    let dados = {
+            Email : $("#emailform").val()
+    }
+
+    fetch("/api/dataentities/NL/documents", {
+        method: 'PATCH',
+               headers: {
+                "Content-Type": "application/json"
+                  },
+        body: JSON.stringify(dados)
+    })
+    .then((res) => {return res})
+    .then(result =>{
+        console.log(result);
+        alert("Inscrição concluída");
+    })
+    .catch(err => console.log(err))
+})
+    
