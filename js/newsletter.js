bcbad05d3bacbcded662576c28a1dc440f643ec2
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-10-27 10:59:16 -0300

    d

--- js/newsletter.js
+++ js/newsletter.js
@@ -39,5 +39,47 @@ $("#commit").click(function (event) {
 })
 
 
+$("#committ").click(function (event) {
+    event.preventDefault();
+  
+
+        let myname = $("#nameformblack").val();
+        let myemail = $("#emailformblack").val();
+        console.log(myname, myemail)
+        __blc['id'] = "7b7cf8b423b8c4472176d1372701ecdd";
+
+        try {
+
+            lc.sendData({
+                evento: "Novo Cadastro Newsletter",
+                nm_email: myemail,
+                vars: {
+                    nome: myname,
+                },
+                vars_json: {
+
+                },
+                lista: {
+                    nm_lista: "Carajas_BlackFriday",
+                    atualizar: "1",
+                    nome: myname
+
+                }
+            });
+
+          alert("Inscrição concluída!")
+
+        } catch (e) {
+            alert("Algo de errado aconteceu :( \n Tente mais tarde")
+        }
+    
+
+
+
+})
+
+
+
+
 
 
