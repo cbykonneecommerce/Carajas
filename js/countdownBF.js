a762dd13339d68459f8088417873b37d7a974f9d
Renata <renata.felismino@cbykonne.com>
2020-10-30 15:47:27 -0300

    a

--- js/countdownBF.js
+++ js/countdownBF.js
@@ -103,4 +103,22 @@ setTimeout(function(){
   $(".dropdown-container").hide();
 
 
+  function calculateHMSleft()
+{
+	//calculate
+	var now = new Date();
+	var hoursleft = 23-now.getHours();
+	var minutesleft = 59-now.getMinutes();
+	var secondsleft = 59-now.getSeconds();
+
+	//format 0 prefixes
+	if(minutesleft<10) minutesleft = "0"+minutesleft;
+	if(secondsleft<10) secondsleft = "0"+secondsleft;
+
+	//display
+	console.log(hoursleft+":"+minutesleft+":"+secondsleft);
+}
+
+calculateHMSleft();
+setInterval(calculateHMSleft, 1000);
 
